5322d5fa1ed7b6d4565972df0ce60f7d31d65a28
Philippe Creux <pcreux@gmail.com>
2010-07-27 09:13:40 -0700

    Require 'rubygems' in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 #ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
 
+require "rubygems"
 require "bundler"
 Bundler.load
 
